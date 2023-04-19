eb28f85d9397ea51e36012183737f3bf08aee796
Sagar Chopra <schopra@firemate.com>
2023-04-19 18:28:25 +0530

    Tagcloud

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -8,10 +8,10 @@ const getPosts = () => {
       try{
 
          //simulate delay
-         await new Promise(resolve => {
-          setTimeout(resolve, 2000)
-        })
-        
+        //  await new Promise(resolve => {
+        //   setTimeout(resolve, 2000)
+        // })
+
         let data = await fetch('http://localhost:3000/posts')
         if(!data.ok){
           throw Error('No Data Available')
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="tag">
         <div v-if="error">{{ error }}</div>
-        <div v-if="posts.length">
+        <div v-if="posts.length" class="layout">
             <PostList :posts="postsWithTag"/>
+            <TagCloud :posts="posts"/>
         </div>
         <div v-else>
             <Spinner/>
@@ -13,6 +14,7 @@
 <script>
 import Spinner from '@/components/Spinner.vue'
 import PostList from '@/components/PostList.vue'
+import TagCloud from '@/components/TagCloud.vue'
 import getPosts from '@/composables/getPosts'
 import { computed } from 'vue'
 import { useRoute}  from 'vue-router'
@@ -29,8 +31,14 @@ export default {
         
         return { posts, error, postsWithTag };
     },
-    components: { PostList, Spinner }
+    components: { PostList, Spinner, TagCloud }
 }
 </script>
 
-<style></style>
+<style>
+.tag{
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+}
+</style>
